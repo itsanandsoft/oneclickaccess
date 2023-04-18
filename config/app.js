5121d46d87bcdcab82623044c26d261fbf31c2c6
Naeem Maqbool <virtualskillsharenetwork@gmail.com>
2023-04-18 23:43:13 +0500

    tray icon and notification

--- config/app.js
+++ config/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const config = {
     protocol:process.env.API_PROTOCOL,
     api_url: process.env.API_PROTOCOL+'://'+process.env.API_BASE_URL,
+    app_name: process.env.APP_NAME,
 };
 
 module.exports = config;
